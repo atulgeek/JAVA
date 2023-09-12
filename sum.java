f1e5e29d7e21194fd3490355f813ed3a16fd4683
Atul Kumar <atulkumargeek@gmail.com>
2023-09-12 20:11:23 +0530

    Sum Prog

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 30;
+        int sum = a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("a + b = " + sum);
+    }
+}
