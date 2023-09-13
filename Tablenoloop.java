5f5a5aa806945838d7226199e137d5368e8f0963
Atul Kumar <atulkumargeek@gmail.com>
2023-09-13 16:10:42 +0530

    next day first commit

---
+++ Tablenoloop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TableNoLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number To Make A Table : ");
+        int a = sc.nextInt();
+        System.out.println(a*1);
+        System.out.println(a*2);
+        System.out.println(a*3);
+        System.out.println(a*4);
+        System.out.println(a*5);
+        System.out.println(a*6);
+        System.out.println(a*7);
+        System.out.println(a*8);
+        System.out.println(a*9);
+        System.out.println(a*10);
+    }
+}
---
+++ radiusofcircle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class radiusofcircle {
+        public static void main(String k []) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the Radius of a Circle : ");
+            Double r = sc.nextDouble();
+            Double a ;
+            a = (22*r*r)/7;
+            System.out.println("Area of Circle is = "+a);
+    }
+    
+}
