6d7c5758e2cd12d682ba3e8891cc9d340c25243a
Atul Kumar <atulkumargeek@gmail.com>
2023-09-16 23:33:17 +0530

    Finally

---
+++ InvertedHalfPyramidPattern180DegreeRoteted.java
@@ -0,0 +1,18 @@
+public class InvertedHalfPyramidPattern180DegreeRoteted {
+    public static void main(String[] args) {
+        int n = 5;
+        //Outer Loop
+        for(int i =1; i<=n; i++){
+        // Inner Loop - Space Print 
+        for(int j =1; j<=n-i; j++){
+            System.out.print(" ");
+        }
+        // Inner Loop - Star Print
+        for(int j =1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+        }
+        
+    }
+}
