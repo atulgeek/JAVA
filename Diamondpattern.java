db7beae3ea0d5ebc3d7d936ad6d12144fd9ced3d
Atul Kumar <atulkumargeek@gmail.com>
2023-09-21 22:45:46 +0530

    hw

--- Diamondpattern.java
+++ Diamondpattern.java
@@ -1,10 +1,27 @@
 public class Diamondpattern {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
+        int n = 7;
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                // spaces
                 System.out.print(" ");
-            }System.out.print("*");
-    }//System.out.println();
-}
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+        // outer loop
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                // spaces
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
 }
